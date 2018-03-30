cd35777485968eed8ce3a4da4de8f9d62931807f
Shun Yao <ysmiracle@hotmail.com>
2018-03-30 04:46:58 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,10 +1,12 @@
 def reformat_languages(languages)
   new_hash = {}
   
-  languages.each do |style, language|
-    new_hash[language][:style] = style
-    language.each do |type, type_value|
-      new_hash[language][type] = type_value
+  languages.each do |style, style_language|
+    style_language.each do |language, language_type|
+      language_type.each do |type, type_value|
+        new_hash[language][:style] = style
+        new_hash[language][type] = type_value
+      end
     end
   end
   
