b013ebac8bb12657a2b287fd3dafb1811b585b64
Shun Yao <ysmiracle@hotmail.com>
2018-03-30 04:58:51 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,8 +5,8 @@ def reformat_languages(languages)
     style_language.each do |language, language_type|
       language_type.each do |type, type_value|
         new_hash[language] = {type => type_value}
-        new_hash[language][:style] = []
-        new_hash[language][:style] = style
+        new_hash[language][:style] << style
+        #new_hash[language][:style] = style
       end
     end
   end
